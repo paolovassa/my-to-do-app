b6a94d0c9418be87c0b5096bc6f58672f5aaf6a0
Paolo Vassallo <paolovassa@virgilio.it>
2025-07-15 15:58:19 +0200

    Concluso le funzioni di validazione di entrambi i form

--- js/login.js
+++ js/login.js
@@ -34,7 +34,7 @@ function checkValidPassword() {
     validPassword = false;
   }
 
-  console.log(validPassword);
+  return validPassword;
 }
 
 function checkValidLogin() {
@@ -62,6 +62,54 @@ function checkValidLogin() {
   if (allValid) loginBtn.removeAttribute("disabled");
 }
 
+function checkValidRegistration() {
+  registerBtn.setAttribute("disabled", "");
+  let allValid = true;
+
+  //Controlliamo che il campo "Username" contenga almeno 6 caratteri
+  let IDslices = registerUsernameInput.getAttribute("id").split("-");
+  let textID = [...IDslices.slice(0, IDslices.length - 1), "constraint"].join(
+    "-"
+  );
+  let controlText = document.querySelector(`#${textID}`);
+
+  if (registerUsernameInput.value.trim().length >= 6) {
+    controlText.classList.add("valid-input");
+  } else {
+    controlText.classList.remove("valid-input");
+    allValid = false;
+  }
+
+  //Controlliamo che il campo "Email" contenga un indirizzo valido
+  IDslices = registerEmailInput.getAttribute("id").split("-");
+  textID = [...IDslices.slice(0, IDslices.length - 1), "constraint"].join("-");
+  controlText = document.querySelector(`#${textID}`);
+
+  if (/[^@]+@[^@]+\.[a-z]+/.test(registerEmailInput.value.trim())) {
+    controlText.classList.add("valid-input");
+  } else {
+    controlText.classList.remove("valid-input");
+    allValid = false;
+  }
+
+  //Controlliamo che il valore del campo password soddisfi tutti i criteri
+  if (
+    checkValidPassword() &&
+    registerPasswordInput.value === confirmPasswordInput.value
+  ) {
+    document
+      .querySelector("#correspondence-register-password")
+      .classList.add("valid-input");
+  } else {
+    allValid = false;
+    document
+      .querySelector("#correspondence-register-password")
+      .classList.remove("valid-input");
+  }
+
+  if (allValid) registerBtn.removeAttribute("disabled");
+}
+
 /*Eseguita al caricamento della pagina. Recupera il valore associato alla chiave 
 "redirectToPanel" nel localStorage e assegna le classi "show" e "active" al pannello che ha 
 come ID il valore recuperato.*/
@@ -80,15 +128,21 @@ function showSelectedPanel() {
   localStorage.removeItem("redirectToPanel");
 }
 
-/* */
-
 var pillsTabContent = document.getElementById("pills-tabContent");
+
+//Elementi del form di accesso
 var loginBtn = document.getElementById("login-btn");
 var loginForm = document.getElementById("login-form");
+
+//Elementi del form di registrazione
 var registerForm = document.getElementById("register-form");
+var registerUsernameInput = document.getElementById("register-username-input");
+var registerEmailInput = document.getElementById("register-email-input");
 var registerPasswordInput = document.getElementById("register-password-input");
+var confirmPasswordInput = document.getElementById("confirm-register-password");
+var registerBtn = document.getElementById("register-btn");
 
 //Event Listeners
 window.addEventListener("DOMContentLoaded", showSelectedPanel);
 loginForm.addEventListener("input", checkValidLogin);
-registerForm.addEventListener("input", checkValidPassword);
+registerForm.addEventListener("input", checkValidRegistration);
